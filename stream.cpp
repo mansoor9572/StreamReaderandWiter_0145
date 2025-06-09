dd0041e2a35b7b5a4ed8438aef16593ea78a4433
mansoor <t.alyamani95@gmail.com>
2025-06-09 18:31:32 +0700

    Create and open the file for writing

--- stream.cpp
+++ stream.cpp
@@ -1,3 +1,18 @@
 #include <fstream>  
 #include <iostream>
 #include <string>
+using namespace std;
+
+
+
+int main()
+{
+    string baris;            // Variable to store each line of input
+    ofstream outfile;        // Output file stream to write to a file
+
+    outfile.open("contohfile.txt");  // Create and open the file for writing
+
+    cout << ">= Menulis file, 'q' untuk keluar" << endl;
+
+   
+}
